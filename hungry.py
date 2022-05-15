1ecc442ba52db624e48effaa61cbece261237eec
Sneharth Bhajani <sneharth@Sneharths-MacBook-Pro.local>
2022-05-15 16:28:27 +0530

    This is an attempt to push the code to thirsty branch

--- hungry.py
+++ hungry.py
@@ -4,3 +4,8 @@ if hungry == "yes":
     print ( "Eat pizza." )
 else:
     print ( "Do you homework." )
+    thirsty = input ( "Are you thirsty?" )
+    if thirsty == "yes":
+        print ( "Drink some water or juice." )
+    else:
+        print ( "Do nothing." )
