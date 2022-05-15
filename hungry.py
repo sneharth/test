3da1fa411d762d696792e08d1857830df31e7527
Sneharth Bhajani <sneharth@Sneharths-MacBook-Pro.local>
2022-05-15 16:32:31 +0530

    Master change

--- hungry.py
+++ hungry.py
@@ -2,5 +2,6 @@ hungry = input ( "Are you hungry?" )
 if hungry == "yes":
     print ( "Eat something.") 
     print ( "Eat pizza." )
+    print ( "Eat fries." )
 else:
     print ( "Do you homework." )
